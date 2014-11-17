c2954be351e89b089b968ac6ce5b8045c0957f86
Xavier Perches <xavierperches@yahoo.com>
2014-11-17 00:35:01 -0700

    perflab-setup/

--- perflab-setup/FilterMain.cpp
+++ perflab-setup/FilterMain.cpp
@@ -162,8 +162,8 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
   else if ( value  > 255 ) { value = 255; }
   output -> color[2][row][col] = value;
       }
-    }
-  }
+   // }
+ // }
 
   cycStop = rdtscll();
   double diff = cycStop - cycStart;
